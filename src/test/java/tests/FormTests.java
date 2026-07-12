18e5c104e7dd923e43b6dca49212efa17fddd106
Paul Tselyuk <pavlik_astap@mail.ru>
2026-07-12 23:57:01 +0300

    Modified and optimized tests

--- src/test/java/tests/FormTests.java
+++ src/test/java/tests/FormTests.java
@@ -2,10 +2,12 @@ package tests;
 
 import com.codeborne.selenide.ScrollIntoViewOptions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Condition.cssValue;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FormTests extends TestBase {
@@ -16,30 +18,35 @@ public class FormTests extends TestBase {
         $("#firstName").setValue("Alex");
         $("#lastName").setValue("Black");
         $("#userEmail").setValue("alex@black.com");
-        $("#gender-radio-1").click();
+        $("#genterWrapper input[value=Male]").click();
         $("#userNumber").setValue("5468484684");
         $("#dateOfBirthInput").click();
-        $("#dateOfBirthInput").sendKeys(Keys.CONTROL + "a");
-        $("#dateOfBirthInput").sendKeys("15 Mar 2000");
-        $("#dateOfBirthInput").pressEnter();
+        $(".react-datepicker__month-select").click();
+        $("option[value='2']").click();
+        $(".react-datepicker__year-select").click();
+        $("option[value='2000']").click();
+        $x("//div[@id='dateOfBirth']//div[.='15']").click();
         $("#subjectsInput").setValue("Hindi").pressEnter();
-        $("#hobbies-checkbox-1").click();
-        $("#currentAddress").setValue("John Doe123 Elm Street, Apt 4BNew York, NY 10001");
-        $("#state svg").scrollIntoView(ScrollIntoViewOptions.instant()).click();
-        $("#react-select-3-option-0").click();
-        $("#city svg").click();
-        $("#react-select-4-option-0").click();
+        $x("//div[@id='hobbiesWrapper']//label[.='Sports']").click();
+        File image = new File("src/test/resources/images/toad.webp");
+        $("#uploadPicture").uploadFile(image);
+        $("#currentAddress").setValue("John Doe 123 Elm Street, Apt 4BNew York, NY 10001");
+        $("#state").scrollIntoView(ScrollIntoViewOptions.instant()).click();
+        $("#state").$(byText("NCR")).click();
+        $("#city").click();
+        $("#city").$(byText("Delhi")).click();
         $("#submit").click();
 
-        $x("//div[@class='table-responsive']//tbody//tr[1]/td[2]").shouldHave(text("Alex Black"));
-        $x("//div[@class='table-responsive']//tbody//tr[2]/td[2]").shouldHave(text("alex@black.com"));
-        $x("//div[@class='table-responsive']//tbody//tr[3]/td[2]").shouldHave(text("Male"));
-        $x("//div[@class='table-responsive']//tbody//tr[4]/td[2]").shouldHave(text("5468484684"));
-        $x("//div[@class='table-responsive']//tbody//tr[5]/td[2]").shouldHave(text("15 March,2000"));
-        $x("//div[@class='table-responsive']//tbody//tr[6]/td[2]").shouldHave(text("Hindi"));
-        $x("//div[@class='table-responsive']//tbody//tr[7]/td[2]").shouldHave(text("Sports"));
-        $x("//div[@class='table-responsive']//tbody//tr[9]/td[2]").shouldHave(text("John Doe123 Elm Street, Apt 4BNew York, NY 10001"));
-        $x("//div[@class='table-responsive']//tbody//tr[10]/td[2]").shouldHave(text("NCR Delhi"));
+        $(".table-responsive").$(byText("Student Name")).sibling(0).shouldHave(text("Alex Black"));
+        $(".table-responsive").$(byText("Student Email")).sibling(0).shouldHave(text("alex@black.com"));
+        $(".table-responsive").$(byText("Gender")).sibling(0).shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).sibling(0).shouldHave(text("5468484684"));
+        $(".table-responsive").$(byText("Date of Birth")).sibling(0).shouldHave(text("15 March,2000"));
+        $(".table-responsive").$(byText("Subjects")).sibling(0).shouldHave(text("Hindi"));
+        $(".table-responsive").$(byText("Hobbies")).sibling(0).shouldHave(text("Sports"));
+        $(".table-responsive").$(byText("Picture")).sibling(0).shouldHave(text("toad.webp"));
+        $(".table-responsive").$(byText("Address")).sibling(0).shouldHave(text("John Doe 123 Elm Street, Apt 4BNew York, NY 10001"));
+        $(".table-responsive").$(byText("State and City")).sibling(0).shouldHave(text("NCR Delhi"));
     }
 
     @Test
@@ -48,19 +55,22 @@ public class FormTests extends TestBase {
         $("#firstName").setValue("Ryan");
         $("#lastName").setValue("Gosling");
         $("#userEmail").setValue("rgosl@gmail.com");
-        $("#gender-radio-1").click();
+        $("#genterWrapper input[value=Male]").click();
         $("#userNumber").setValue("1122334455");
         $("#dateOfBirthInput").click();
-        $("#dateOfBirthInput").sendKeys(Keys.CONTROL + "a");
-        $("#dateOfBirthInput").sendKeys("12 Nov 1980");
+        $(".react-datepicker__month-select").click();
+        $("option[value='10']").click();
+        $(".react-datepicker__year-select").click();
+        $("option[value='1980']").click();
+        $x("//div[@id='dateOfBirth']//div[.='12']").click();
         $("#submit").scrollIntoView(ScrollIntoViewOptions.instant());
         $("#submit").click();
 
-        $x("//div[@class='table-responsive']//tbody//tr[1]/td[2]").shouldHave(text("Ryan Gosling"));
-        $x("//div[@class='table-responsive']//tbody//tr[2]/td[2]").shouldHave(text("rgosl@gmail.com"));
-        $x("//div[@class='table-responsive']//tbody//tr[3]/td[2]").shouldHave(text("Male"));
-        $x("//div[@class='table-responsive']//tbody//tr[4]/td[2]").shouldHave(text("1122334455"));
-        $x("//div[@class='table-responsive']//tbody//tr[5]/td[2]").shouldHave(text("12 November,1980"));
+        $(".table-responsive").$(byText("Student Name")).sibling(0).shouldHave(text("Ryan Gosling"));
+        $(".table-responsive").$(byText("Student Email")).sibling(0).shouldHave(text("rgosl@gmail.com"));
+        $(".table-responsive").$(byText("Gender")).sibling(0).shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).sibling(0).shouldHave(text("1122334455"));
+        $(".table-responsive").$(byText("Date of Birth")).sibling(0).shouldHave(text("12 November,1980"));
     }
 
     @Test
@@ -72,7 +82,7 @@ public class FormTests extends TestBase {
         $("#firstName").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
         $("#lastName").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
         $("#userNumber").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
-        $("#gender-radio-1").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $("#genterWrapper input[value=Male]").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
     }
 
     @Test
