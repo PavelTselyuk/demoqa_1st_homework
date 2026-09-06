cff008cf6262de0da0782fe8875863b3b3d2ab7d
Paul Tselyuk <pavlik_astap@mail.ru>
2026-09-06 18:52:27 +0300

    Added possibility

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -43,7 +43,8 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
 //        Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000; // default 4000
-        Configuration.remote = url;
+//        Configuration.remote = url;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
