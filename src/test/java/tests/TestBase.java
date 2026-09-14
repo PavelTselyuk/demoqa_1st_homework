80ddeb592d04ee126eabffc2973e9f7b06e77baa
Paul Tselyuk <pavlik_astap@mail.ru>
2026-09-14 23:15:29 +0300

    added few parameters

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -33,8 +33,8 @@ public class TestBase {
         String password = System.getProperty("password");
         String selenoidAddress = System.getProperty("selenoidAddress");
         Configuration.browser = System.getProperty("browser");
-//        Configuration.browserVersion = "144.0";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.browserSize = System.getProperty("browserSize");
         Configuration.baseUrl = System.getProperty("baseUrl");
 //        Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000; // default 4000
